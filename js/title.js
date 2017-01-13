a095fe9d6cb9fd38688cfc9243c8d00a68352073
jzz7280 <1563382649@qq.com>
2017-01-14 02:12:53 +0800

    initial commint

---
+++ js/title.js
@@ -0,0 +1,13 @@
+[{
+		"index":2,
+		"title":"一些css效果 [ 请使用新版chrome ]",
+		"href":"demo/ripple.html",
+		"make":"css",
+		"time":"2017-1-12"
+	},
+	{
+		"index":3,
+		"title":"美化404图片",
+		"make":"css",
+		"time":"2017-1-13"
+	}]
