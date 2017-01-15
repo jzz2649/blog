02e5aa3bf4063837bc56c7f665e0585b7710041a
jzz7280 <1563382649@qq.com>
2017-01-15 18:32:09 +0800

    add demo

--- js/title.js
+++ js/title.js
@@ -16,4 +16,11 @@
 		"title":"关于移动端border 1px",
 		"make":"css",
 		"time":"2017-1-14"
+	},
+	{
+		"index":5,
+		"title":"仿某电商网站(移动端),作参考,其实我是拒绝拿出的,之前做的demo,无意间看到",
+		"href":"demo/demo4/index.html",
+		"make":"html",
+		"time":"2017-1-15"
 	}]
