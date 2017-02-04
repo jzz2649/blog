82d6dbaf89db554104f1e6b0ce9fdbb9d68d7614
jzz7280 <1563382649@qq.com>
2017-02-05 01:12:35 +0800

    edit title.js

--- js/title.js
+++ js/title.js
@@ -1,41 +1,41 @@
 [{
-		"index":2,
-		"title":"一些css效果 [ 请使用新版chrome ]",
-		"href":"demo/ripple.html",
-		"make":"css",
-		"time":"2017-1-12"
-	},
-	{
-		"index":3,
-		"title":"美化404图片",
-		"make":"css",
-		"time":"2017-1-13"
-	},
-	{
-		"index":4,
-		"title":"关于移动端border 1px",
-		"make":"css",
-		"time":"2017-1-14"
-	},
-	{
-		"index":5,
-		"title":"仿某电商网站(移动端),作参考,其实我是拒绝拿出的,之前做的demo,无意间看到",
-		"href":"demo/demo4/index.html",
-		"make":"html",
-		"time":"2017-1-15"
-	},
-	{
-		"index":6,
-		"title":"ios dom元素滚动到可视区域不可见bug",
-		"make":"js",
-		"time":"2017-1-17"
-	},
-	{
-		"index":7,
-		"title":"requestAnimationFrame之切换tab暂停问题",
-		"make":"js",
-		"time":"2017-1-17"
-	},
+    "index":2,
+    "title":"一些css效果 [ 请使用新版chrome ]",
+    "href":"demo/ripple.html",
+    "make":"css",
+    "time":"2017-1-12"
+  },
+  {
+    "index":3,
+    "title":"美化404图片",
+    "make":"css",
+    "time":"2017-1-13"
+  },
+  {
+    "index":4,
+    "title":"关于移动端border 1px",
+    "make":"css",
+    "time":"2017-1-14"
+  },
+  {
+    "index":5,
+    "title":"仿某电商网站(移动端),作参考,其实我是拒绝拿出的,之前做的demo,无意间看到",
+    "href":"demo/demo4/index.html",
+    "make":"html",
+    "time":"2017-1-15"
+  },
+  {
+    "index":6,
+    "title":"ios dom元素滚动到可视区域不可见bug",
+    "make":"js",
+    "time":"2017-1-17"
+  },
+  {
+    "index":7,
+    "title":"requestAnimationFrame之切换tab暂停问题",
+    "make":"js",
+    "time":"2017-1-17"
+  },
   {
     "index":8,
     "title":"css sticky footer",
